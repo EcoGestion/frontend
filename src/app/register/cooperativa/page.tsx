6c8f42c8ece9ef44a8eb3c4e264af484a9480dd5
Teo Correa Maya <teo-correa@hotmail.com>
2024-07-05 16:47:46 -0300

    Add form to register

--- src/app/register/cooperativa/page.tsx
+++ src/app/register/cooperativa/page.tsx
@@ -11,7 +11,8 @@ const SignUpCooperativa = () => {
   const [password, setPassword] = useState('');
   const router = useRouter();
 
-  const handleSignUp = async () => {
+  const handleSignUp = async (e) => {
+    e.preventDefault();
     console.log(name, email, password)
     createUserWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
@@ -32,13 +33,14 @@ const SignUpCooperativa = () => {
       <div className="bg-[rgb(146,164,190)] p-10 rounded-lg shadow-lg">
         <h1 className="text-4xl font-bold text-gray-800 mb-4 text-center">Eco Gestion</h1>
         <h2 className="text-2xl mb-6 text-center">Registro de usuario - Cooperativa</h2>
-        <div className="flex flex-col items-center gap-4">
+        <form onSubmit={handleSignUp} className="flex flex-col items-center gap-4">
           <input
             type="text"
             placeholder="Nombre de la cooperativa"
             className="border border-gray-300 p-2 rounded text-black w-64"
             value={name}
             onChange={(e) => setName(e.target.value)}
+            required
           />
           <input
             type="email"
@@ -46,6 +48,7 @@ const SignUpCooperativa = () => {
             className="border border-gray-300 p-2 rounded text-black w-64"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
+            required
           />
           <input
             type="password"
@@ -53,19 +56,20 @@ const SignUpCooperativa = () => {
             className="border border-gray-300 p-2 rounded text-black w-64"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
+            required
           />
 
           <div className="flex gap-4 mt-4">
-            <GreenRoundedButton onClick={handleSignUp} buttonTitle="Crear Cuenta" />
+            <GreenRoundedButton buttonTitle="Crear Cuenta" type='submit' />
           <button
+            type='button'
             className="bg-gray-500 hover:bg-gray-600 text-white font-semibold py-2 px-6 rounded shadow-md transition duration-300 " 
             onClick={handle_back}
           >
             Volver atrás
           </button>
           </div>
-        
-        </div>
+        </form>
       </div>
     </div>
   );
