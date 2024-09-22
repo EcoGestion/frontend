dc06dd896a53e6e403c4b306cd2d4dec5c8087ec
Franco Tosoni Márquez <74151058+francotosoni@users.noreply.github.com>
2024-09-22 12:31:10 -0300

    Refactor pedidos (#14)
    * redesign
    
    * add table and fix details page
    
    * fix imports
    
    * fix types
    
    * fix types

--- src/api/apiService.js
+++ src/api/apiService.js
@@ -17,7 +17,17 @@ export const getUserById = async (userId) => {
   return response.data;
 };
 
-export const getOrdersById = async (userId) => {
+export const getCoopOrdersById = async (userId) => {
   const response = await axios.get(`${API_BASE_URL}/users/${userId}/coop_requests`);
+  return response.data;
+};
+
+export const getGeneratorOrdersById = async (userId) => {
+  const response = await axios.get(`${API_BASE_URL}/users/${userId}/waste_collection_requests`);
+  return response.data;
+};
+
+export const getOrderById = async (orderId) => {
+  const response = await axios.get(`${API_BASE_URL}/waste_requests/${orderId}`);
   return response.data;
 };
