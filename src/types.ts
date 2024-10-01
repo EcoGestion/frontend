b2eb48bc493c26c32928734867d70240a8c9d08d
Teo Correa Maya <teo-correa@hotmail.com>
2024-10-01 00:09:28 -0300

    Use Coop resources for route cration

--- src/types.ts
+++ src/types.ts
@@ -77,3 +77,5 @@ export interface WasteCollectionRequest {
   waste_quantities: {waste_type: string, quantity: number}[];
   status: 'OPEN' | 'PENDING' | 'ACCEPTED' | 'REJECTED' | 'COMPLETED';
 }
+
+export type WasteCollectionRequests = WasteCollectionRequest[];
