94a8f2051b188e01bec542166cdbfe4ee41ab6f7
Teo Correa Maya <teo-correa@hotmail.com>
2024-11-06 15:17:08 -0300

    Fix metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,18 @@
-'use client'
 import 'bootstrap/dist/css/bootstrap.css';
 import { Inter } from "next/font/google";
 import "./globals.css";
 import "./styles/app_buttons.css";
 import "./styles/app_forms.css";
-import { Provider } from 'react-redux';
-import { store, persistor } from '../state/store';
-import { PersistGate } from 'redux-persist/integration/react';
 import {NextUIProvider} from '@nextui-org/react'
 import { UserProvider } from '../state/userProvider'
 import React from 'react';
+import type { Metadata } from "next";
+import ReduxProvider from './ReduxProvider';
+
+export const metadata: Metadata = {
+    title: "EcoGestion",
+    description: "Conectando cooperativas con generadores",
+};
 
 const inter = Inter({ subsets: ["latin"] });
 export default function RootLayout({
@@ -21,13 +24,11 @@ export default function RootLayout({
     <UserProvider>
     <html lang="en">
       <body className={inter.className}>
-        <Provider store={store}>
-          <PersistGate loading={null} persistor={persistor}>
+        <ReduxProvider>
             <NextUIProvider>
               {children}
             </NextUIProvider>
-          </PersistGate>
-        </Provider>
+        </ReduxProvider>
       </body>
     </html>
     </UserProvider>
