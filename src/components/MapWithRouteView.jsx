0a107c242c6078536e1de0f94978903911278536
Teo Correa Maya <teo-correa@hotmail.com>
2024-12-18 12:28:58 -0300

    https for route

--- src/components/MapWithRouteView.jsx
+++ src/components/MapWithRouteView.jsx
@@ -7,7 +7,7 @@ import axios from 'axios';
 
 const fetchRoute = async (start, end) => {
   if (!start || !end) return [];
-  const response = await axios.get(`http://router.project-osrm.org/route/v1/driving/${start[1]},${start[0]};${end[1]},${end[0]}?overview=full&geometries=geojson`);
+  const response = await axios.get(`https://router.project-osrm.org/route/v1/driving/${start[1]},${start[0]};${end[1]},${end[0]}?overview=full&geometries=geojson`);
   return response.data.routes[0].geometry.coordinates;
 };
 
